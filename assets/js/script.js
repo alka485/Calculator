29e245854a24bed5a33e5ced9594af5d35899c24
alkasah <alkasah485@gmail.com>
2022-12-07 07:12:04 -0800

    updation in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 console.log("hello");
+
+var input    = document.getElementById('.input');
+var number   = document.getElementById('.numbers');
+var operator = document.getElementById('.operators');
+var result   = document.getElementById('result');
+var clear    = document.getElementById('clear');
+var resultDisplayed = false;
