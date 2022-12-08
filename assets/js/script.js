efdff71b79d4990201748027d7013b165a3b3cec
alkasah <alkasah485@gmail.com>
2022-12-08 08:49:30 -0800

    updation in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 console.log("hello");
 
 var input    = document.getElementById('.input');
-var number   = document.getElementById('.numbers');
+console.log(input);
+var number   = document.getElementById('.numbers div');
+console.log(number);
 var operator = document.getElementById('.operators');
-var result   = document.getElementById('result');
-var clear    = document.getElementById('clear');
+console.log(operator);
+var result   = document.getElementById('#result');
+console.log(result);
+var clear    = document.getElementById('#clear');
 var resultDisplayed = false;
+
+for(var i = 0 ; i < number.length; i++){
+    number[i].addEventListener("click" , function(e){
+        var currentString = input.innerHTML;
+        console.log(currentString);
+        var lastChar = currentString[currentString.length -1]
+        if(resultDisplayed === false){
+            input.innerHTML += e.target.innerHTML;
+        }
+    })
+
+}
